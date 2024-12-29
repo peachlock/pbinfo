730f7c74d7b9488e8c50136c95411ca9cd5671c7
peachlock <sargema.copil@gmail.com>
2024-12-29 14:49:53 +0200

    finalizare P335

---
+++ probleme/335.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, s = 0, i;
+  cin >> n;
+
+  for (i = 1; i <= n; i++)
+  {
+    if (i % 2 != 0)
+    {
+      s = s + i * (i + 1);
+    }
+
+    else
+    {
+      s = s - i * (i + 1);
+    }
+  }
+
+  cout << "Rezultatul este " << s << " ";
+
+  return 0;
+}

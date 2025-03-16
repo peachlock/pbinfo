cc2581c2fe833e2c32b24d0f1ed8d2516ef6ff33
peachlock <sargema.copil@gmail.com>
2025-03-16 20:14:44 +0200

    interclasare cu fisiere

---
+++ probleme/Fisiere/#241/#241.cpp
@@ -0,0 +1,51 @@
+// Se dau două şiruri a şi b, cu n, respectiv m elemente, numere naturale, ordonate crescător.
+// Să se construiască un al treilea şir, c, care să conţină, în ordine crescătoare, elementele din şirurile a şi b.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream fin("interclasare.in");
+ofstream fout("interclasare.out");
+
+int main()
+{
+  int n, m, a[1001], b[1001], c[2001], i, j, k;
+
+  // citim din fisier numerele si sirurile
+  fin >> n;
+  for (i = 1; i <= n; i++)
+    fin >> a[i];
+
+  fin >> m;
+  for (i = 1; i <= n; i++)
+    fin >> b[i];
+  fin.close();
+
+  // incepem interclasarea
+  i = j = 1;
+  k = 0;
+  while (i <= n && j <= m)
+  {
+    if (a[i] <= b[j])
+    {
+      c[++k] = a[i++];
+    }
+    else
+    {
+      c[++k] = b[j++];
+    }
+  }
+
+  while (i <= n)
+    c[++k] = a[i++];
+
+  while (j <= m)
+    c[++k] = b[j++];
+
+  // afisarea noului sir
+  for (i = 1; i <= k; i++)
+    fout << c[i] << " ";
+
+  return 0;
+}
